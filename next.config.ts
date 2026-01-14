de4990951527e5c47bd24f2453f42b1071dad11e
Aphisit2546 <apisit9048@gmail.com>
2026-01-14 11:43:08 +0700

    v1.0.8

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "192.168.24.148",
+  ],
 };
 
 export default nextConfig;
